704e7cce3e7ac2cac427e52fe43efbfd7bdf2774
Ter-Stepanyan <artak.ter-stepanyan@team.gg>
2024-02-08 11:28:15 +0400

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Artak Ter-Stepanyan on 08.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
