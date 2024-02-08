8929d281aded70b08d0f8b8367a1fdb0c648eb41
Ter-Stepanyan <artak.ter-stepanyan@team.gg>
2024-02-08 14:05:36 +0400

    To Do app is created with database using Core Data.

---
+++ ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListApp
+//
+//  Created by Artak Ter-Stepanyan on 08.02.24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
---
+++ ToDoListItem/ToDoListItem+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  ToDoListItem+CoreDataProperties.swift
+//  ToDoListApp
+//
+//  Created by Artak Ter-Stepanyan on 08.02.24.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoListItem {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoListItem> {
+        return NSFetchRequest<ToDoListItem>(entityName: "ToDoListItem")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var createdAt: Date?
+
+}
+
+extension ToDoListItem : Identifiable {
+
+}
